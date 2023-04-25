c86f2594a3af6eb80c025a172aac0fa88938e226
kinjo kose <c0b220522d@edu.teu.ac.jp>
2023-04-25 15:47:38 +0900

    練習問題5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -12,6 +12,23 @@ delta = {
 }#↓トップレベルの後は空行２行
 
 
+def check_bound(scr_rct :pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面外or画面内を判定し、真理値タプルを返す
+    引数１　画面surfceのrect
+    引数２　コウカトンまたは、爆弾のsurfceのrect
+    戻り値：横方向、縦方向のはみ出し判定結果（画面内：True/画面外：False）
+    """
+    yoko, tate = True, True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko,tate
+
+    
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -43,16 +60,22 @@ def main():
         for k, mv in delta.items():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
-
-        if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0,-1))
-
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):#練習５
+            for k, mv in delta.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])
+        
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)#こうかとん画像を900,440の位置にblit(貼り付ける)
         #screen.blit(bb_img, [x, y])　#練習問題２
         bb_rct.move_ip(vx, vy)
-        screen.blit(bb_img,bb_rct) #練習問題３
         
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko: #横方向にはみ出ていたら
+            vx *= -1
+        if not tate: #縦方向にはみ出ていたら
+            vy *= -1
+        screen.blit(bb_img,bb_rct) #練習問題３
         pg.display.update()
         clock.tick(1000)
 
