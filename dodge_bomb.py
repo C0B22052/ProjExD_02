f0c71edd013cf49d7eb723d4d20a57234b23d40a
kinjo kose <c0b220522d@edu.teu.ac.jp>
2023-04-25 14:15:09 +0900

    練習問題1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import sys
 
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -8,7 +9,13 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    #爆弾を作る
+    bb_img = pg.Surface((20,20)) #黒い正方形を作る
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)#中心に赤い円を描画
+    bb_img.set_colorkey((0,0,0))#黒を透過させる
     tmr = 0
+
+
 
     while True:
         for event in pg.event.get():
@@ -17,8 +24,8 @@ def main():
 
         tmr += 1
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
-
+        screen.blit(kk_img, [900, 400])#こうかとん画像を900,440の位置にblit(貼り付ける)
+        screen.blit(bb_img, [600, 200])
         pg.display.update()
         clock.tick(1000)
 
